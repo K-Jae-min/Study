3daaea4e7bc6873ea24a15918dd8bfe424ea4e12
재민 김 <kjaemin0122@naver.com>
2022-12-08 23:39:21 +0900

    css,js edu

---
+++ calss/03-javascript/01-variable.js
@@ -0,0 +1,5 @@
+const name = "김재민";
+
+name = "잡스";
+
+console.log(name);
---
+++ calss/03-javascript/연습-04-object-in-arr.js
@@ -0,0 +1,14 @@
+let classmate = [
+  { name: "철수", age: 13, school: "다람쥐초" },
+  { name: "영희", age: 11, school: "얼룩말초" },
+  { name: "훈이", age: 10, school: "거북이초" },
+];
+// undefined
+classmate;
+// (3) [{…}, {…}, {…}]
+classmate.length;
+// 3
+classmate[0].school;
+// '다람쥐초'
+classmate[2].age;
+// 10

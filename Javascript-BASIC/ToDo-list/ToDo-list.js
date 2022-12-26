ea073238fa503deb79aaa2d51c1a5b6cf7993d3c
재민 김 <kjaemin0122@naver.com>
2022-12-26 17:26:02 +0900

    Javascript 이벤트 리스너 활용 태그 삭제

--- Javascript-BASIC/ToDo-list/ToDo-list.js
+++ Javascript-BASIC/ToDo-list/ToDo-list.js
@@ -1,16 +1,47 @@
 const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+
+const createTodo = function () {
+  const newLi = document.createElement("li");
+  const newSpan = document.createElement("span");
+  const newBtn = document.createElement("button");
+
+  newBtn.addEventListener("click", () => {
+    newLi.classList.toggle("complete");
+  });
+  newLi.addEventListener("dblclick", () => {
+    newLi.remove();
+  });
+
+  newSpan.textContent = todoInput.value;
+  todoList.appendChild(newLi);
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+  todoInput.value = "";
+  saveItmesFn();
+};
 
 const keyCodeCheck = function () {
-  if (window.event.keyCode === 13 && todoInput.value !== "") {
-    const todoList = document.querySelector("#todo-list");
-    const newLi = document.createElement("li");
-    const newSpan = document.createElement("span");
-
-    newSpan.textContent = todoInput.value;
-    todoList.appendChild(newLi);
-    newLi.appendChild(newSpan);
-    todoInput.value = "";
-
-    console.log(todoList);
+  if (window.event.keyCode === 13 && todoInput.value) {
+    createTodo();
   }
+};
+
+const deleteAll = function () {
+  const liList = document.querySelectorAll("li");
+  for (let i = 0; i < liList.length; i++) {
+    liList[i].remove();
+  }
+};
+
+const saveItmesFn = function () {
+  const saveItmes = [];
+  for (let i = 0; i < todoList.children.length; i++) {
+    const todoObj = {
+      contents: todoList.children[i].querySelector("span").textContent,
+      complete: todoList.children[i].classList.contains("complete"),
+    };
+    saveItmes.push(todoObj);
+  }
+  console.log(saveItmes);
 };

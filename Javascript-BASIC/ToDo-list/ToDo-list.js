60982141dea9e1d3d4f97dfafd9885aec40fa5fd
재민 김 <kjaemin0122@naver.com>
2022-12-28 03:06:11 +0900

    JavaScirpt localStorage로 렌더링 딜레이 해결

--- Javascript-BASIC/ToDo-list/ToDo-list.js
+++ Javascript-BASIC/ToDo-list/ToDo-list.js
@@ -4,6 +4,8 @@ const todoList = document.querySelector("#todo-list");
 
 const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
 
+const savedWeatherData = JSON.parse(localStorage.getItem("saved-weather"));
+
 const createTodo = function (storageData) {
   let todoContents = todoInput.value;
   if (storageData) {
@@ -81,8 +83,13 @@ const weatherDataActive = function ({ location, weather }) {
   const weatherMainList = ["Clear", "Clouds", "Drizzle", "Rain", "Snow", "Thunderstorm"];
   weather = weatherMainList.includes(weather) ? weather : "Fog";
   const locationNameTag = document.querySelector("#location-name-tag");
+
   locationNameTag.textContent = location;
   document.body.style.backgroundImage = `url("./img/${weather}.jpg")`;
+
+  if (!savedWeatherData || savedWeatherData.location !== location || savedWeatherData.weather !== weather) {
+    localStorage.setItem("saved-weather", JSON.stringify({ location, weather }));
+  }
 };
 
 const weatherSearch = function ({ latitude, longitude }) {
@@ -118,6 +125,10 @@ const aksForLocation = function () {
   });
 };
 aksForLocation();
+
+if (savedWeatherData) {
+  weatherDataActive(savedWeatherData);
+}
 
 // const promiseTest = function () {
 //   return new Promise((resolver, reject) => {
